13ff2f8f3e92365b7a8da22df8276a78a57cc3c4
anantkaushik <anant.kaushik2@gmail.com>
2016-09-01 22:36:29 +0530

    Using Power Function.

---
+++ POWER.C
@@ -0,0 +1,12 @@
+// Program to find the third power of two using Power Function.
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+void main()
+{
+int a=2,b=3,c;
+clrscr();
+c=pow(a,b);
+printf("%d raised to %d is %d",a,b,c);
+getch();
+}
