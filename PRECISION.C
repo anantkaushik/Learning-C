d52cef46ca17910a3c8639098bc19b1abaa4ce3c
anantkaushik <anant.kaushik2@gmail.com>
2016-09-02 22:28:20 +0530

    Program to demonstrate the use of Precision Specifiers.

---
+++ PRECISION.C
@@ -0,0 +1,14 @@
+// Program to demonstrate the use of Preciion Specifiers.
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+float x=123.45678;
+clrscr();
+printf("\n %.1f",x);
+printf("\n %.2f",x);
+printf("\n %.3f",x);
+printf("\n %.4f",x);
+printf("\n %.5f",x);
+getch();
+}
