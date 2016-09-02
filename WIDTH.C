47138e74983c30ffc26b212fb420b9eb566277b2
anantkaushik <anant.kaushik2@gmail.com>
2016-09-02 22:29:40 +0530

    Program to demonstrate the Width Specifiers.

---
+++ WIDTH.C
@@ -0,0 +1,12 @@
+// Program to demonstrate the use of width specifiers.
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int x=1,y=2;
+clrscr();
+printf("Heading:");
+printf("\n %3d.Sub Heading.",x);
+printf("\n %6d.Sub Sub Heading.",y);
+getch();
+}
