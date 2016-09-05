d7637e3219cc7b27d1473eabf0fe7ff1dbab24bf
anantkaushik <anant.kaushik2@gmail.com>
2016-09-05 18:48:01 +0530

    Program to display the data in the tabular form.

---
+++ TABLE.C
@@ -0,0 +1,11 @@
+// Program to display the data in the tabular form.
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+printf("\n1992\t17421");
+printf("\n1993\t29210");
+printf("\n1994\t100523");
+getch();
+}
