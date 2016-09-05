5df11d55a3e3681ed29bd968efb485d9d6981667
anantkaushik <anant.kaushik2@gmail.com>
2016-09-05 18:45:12 +0530

    Program to display your Name,DOB and Address.

---
+++ INFO.C
@@ -0,0 +1,11 @@
+// Program to display your Name,DOB and Address.
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+printf("Name: Anant Kaushik");
+printf("\nDate of Birth: 28 August 1998");
+printf("\nAddress: Rz-62 Street No. 15 Vashisth Park,New Delhi");
+getch();
+}
