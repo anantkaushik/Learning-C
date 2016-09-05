472aa0c10b87d8720899bae4e8268fb98d02f8c6
anantkaushik <anant.kaushik2@gmail.com>
2016-09-05 18:49:24 +0530

    Program to display the marks of 3 Subjects in the tabular form.

---
+++ TABLE..C
@@ -0,0 +1,13 @@
+/* Program to display the marks of
+Physics, Maths, Chemistry in the tabular form.*/
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+printf("Subject\t\tMarks");
+printf("\nMaths\t\t90");
+printf("\nPhysics\t\t77");
+printf("\nChemistry\t\t69");
+getch();
+}
