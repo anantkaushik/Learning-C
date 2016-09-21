8f9a89b4a703e40627172377f41c2c35276658ed
anantkaushik <anant.kaushik2@gmail.com>
2016-09-21 22:12:57 +0530

    Program to display first 10 natural no.

---
+++ NATURAL_NO.C
@@ -0,0 +1,14 @@
+// Program to display first 10 natural no.
+#include<conio.h>
+#include<stdio.h>
+void main()
+{
+	int a;
+	clrscr();
+	printf("First ten natural no. are");
+	for(a=1;a<=10;a++)
+	{
+		printf("\n %d",a);
+	}
+	getch();
+}
